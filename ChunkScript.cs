df9e51dce99375de16424beb2d71979420d70e21
harakka <harakka@myrskynkantaja.org>
2014-05-31 16:16:57 +0300

    Only visible geometry is generated

--- ChunkScript.cs
+++ ChunkScript.cs
@@ -23,12 +23,19 @@ public class ChunkScript : MonoBehaviour {
 	float tUnit = 0.125f;
 	Vector2 tGrass = new Vector2(0,7);
 	Vector2 tMud = new Vector2(1,7);
-	int squareCount;
+	int quadCount;
 	MeshCollider col;
 	byte[,,] blocks;
 
+	public byte BlockAt(int x, int y, int z) {
+		if (x < 0 || x >= Size.x || y < 0 || y >= Size.y || z < 0 || z >= Size.z)
+			return 0;
+		else
+			return blocks[x,y,z];
+	}
+
 	public void Init(Vector3 size, Vector2 position) {
-		Debug.Log("Chunk " + Position + " initializing");
+		//Debug.Log("Chunk " + Position + " initializing");
 		Size = size;
 		Position = position;
 		blocks = new byte[(int)Size.x,(int)Size.y,(int)Size.z];
@@ -40,14 +47,12 @@ public class ChunkScript : MonoBehaviour {
 	}
 
 	public void Generate() {
-		//Debug.Log("Chunk: generating " + Size + " blocks at " + Position);
-		//GenerateTerrainData();
 		BuildMesh();
 		UpdateMesh();
 	}
 
 	void GenerateTerrainData() {
-		Debug.Log("Chunk " + Position + " generating blocks");
+		//Debug.Log("Chunk " + Position + " generating blocks");
 		for (int x = 0; x < blocks.GetLength(0); x++) {
 			for (int z = 0; z < blocks.GetLength(2); z++) {
 				var noise = Mathf.FloorToInt(Mathf.PerlinNoise((float)((x+Position.x)/Size.x), (float)(z+Position.y)/Size.z)*Size.y);
@@ -71,91 +76,107 @@ public class ChunkScript : MonoBehaviour {
 		
 		col.sharedMesh = mesh;
 		
-		squareCount = 0;
+		quadCount = 0;
 		NewVertices.Clear();
 		NewTriangles.Clear();
 		NewUv.Clear();
 	}
 
-	void GenBlock(int x, int y, int z, Vector2 texture1, Vector2 texture2) {
-		// Cube's corner coordinates
-		Vector3 point0 = new Vector3 (x  , y  , z+1 );
-		Vector3 point1 = new Vector3 (x + 1 , y  , z+1 );
-		Vector3 point2 = new Vector3 (x + 1 , y , z );
-		Vector3 point3 = new Vector3 (x  , y , z );
-
-		Vector3 point4 = new Vector3 (x  , y-1  , z+1 );
-		Vector3 point5 = new Vector3 (x + 1 , y-1  , z+1 );
-		Vector3 point6 = new Vector3 (x + 1 , y-1 , z );
-		Vector3 point7 = new Vector3 (x  , y-1 , z );
-
-		NewVertices.AddRange(new Vector3[] {
-			point0, point1, point2, point3,
-			point0, point3, point7, point4,
-			point3, point2, point6, point7,
-			point2, point1, point5, point6,
-			point1, point0, point4, point5,
-			point7, point6, point5, point4});
+	void GenFaceCommon(Vector2 tex) {
+		// Add faces for the vertices we just added
+		NewTriangles.AddRange(new int[]{
+			quadCount*4+0, quadCount*4+1, quadCount*4+2,
+			quadCount*4+0, quadCount*4+2, quadCount*4+3});
 
 		// UV region coordinates
-		Vector2 T1uv00 = new Vector2 (tUnit * texture1.x, tUnit * texture1.y);
-		Vector2 T1uv10 = new Vector2 (tUnit * texture1.x + tUnit, tUnit * texture1.y);
-		Vector2 T1uv01 = new Vector2 (tUnit * texture1.x, tUnit * texture1.y + tUnit);
-		Vector2 T1uv11 = new Vector2 (tUnit * texture1.x + tUnit, tUnit * texture1.y + tUnit);
-
-		Vector2 T2uv00 = new Vector2 (tUnit * texture2.x, tUnit * texture2.y);
-		Vector2 T2uv10 = new Vector2 (tUnit * texture2.x + tUnit, tUnit * texture2.y);
-		Vector2 T2uv01 = new Vector2 (tUnit * texture2.x, tUnit * texture2.y + tUnit);
-		Vector2 T2uv11 = new Vector2 (tUnit * texture2.x + tUnit, tUnit * texture2.y + tUnit);
-
-
 		NewUv.AddRange(new Vector2[] {
-			T2uv01, T2uv11, T2uv10, T2uv00,
-			T1uv01, T1uv11, T1uv10, T1uv00,
-			T1uv01, T1uv11, T1uv10, T1uv00,
-			T1uv01, T1uv11, T1uv10, T1uv00,
-			T1uv01, T1uv11, T1uv10, T1uv00,
-			T1uv01, T1uv11, T1uv10, T1uv00
+			new Vector2 (tUnit * tex.x, tUnit * tex.y + tUnit),
+			new Vector2 (tUnit * tex.x + tUnit, tUnit * tex.y + tUnit),
+			new Vector2 (tUnit * tex.x + tUnit, tUnit * tex.y),
+			new Vector2 (tUnit * tex.x, tUnit * tex.y),
 		});
 
-		// Explanation: squareCount = number of currently existing "squares" (actually blocks)
-		// *24 = there's 24 vertices per block
-		// So squareCount*24 = vertices of current block
-		// The 4*1, 4*2 etc gives us the vertices of that particular face.
-		// Top = 0, Left = 1, Front = 2, etc.
+		quadCount++;
+	}
 
-		NewTriangles.AddRange(new int[]{
-			// Top
-			squareCount*24+0, squareCount*24+1, squareCount*24+2,
-			squareCount*24+0, squareCount*24+2, squareCount*24+3,			
-			
-			// Left
-			squareCount*24+0 + 4 * 1, squareCount*24+1 + 4 * 1, squareCount*24+2 + 4 * 1,
-			squareCount*24+0 + 4 * 1, squareCount*24+2 + 4 * 1, squareCount*24+3 + 4 * 1,
-			
-			// Front
-			squareCount*24+0 + 4 * 2, squareCount*24+1 + 4 * 2, squareCount*24+2 + 4 * 2,
-			squareCount*24+0 + 4 * 2, squareCount*24+2 + 4 * 2, squareCount*24+3 + 4 * 2,
-			
-			// Right
-			squareCount*24+0 + 4 * 3, squareCount*24+1 + 4 * 3, squareCount*24+2 + 4 * 3,
-			squareCount*24+0 + 4 * 3, squareCount*24+2 + 4 * 3, squareCount*24+3 + 4 * 3,
-			
-			// Back
-			squareCount*24+0 + 4 * 4, squareCount*24+1 + 4 * 4, squareCount*24+2 + 4 * 4,
-			squareCount*24+0 + 4 * 4, squareCount*24+2 + 4 * 4, squareCount*24+3 + 4 * 4,
-			
-			// Bottom
-			squareCount*24+0 + 4 * 5, squareCount*24+1 + 4 * 5, squareCount*24+2 + 4 * 5,
-			squareCount*24+0 + 4 * 5, squareCount*24+2 + 4 * 5, squareCount*24+3 + 4 * 5,
-		});
+	void GenFaceTop(Vector3 pos, byte block) {
+		NewVertices.AddRange(new Vector3[] {
+			new Vector3 (pos.x, pos.y, pos.z+1 ),
+			new Vector3 (pos.x + 1 , pos.y  , pos.z+1 ),
+			new Vector3 (pos.x + 1 , pos.y , pos.z ),
+			new Vector3 (pos.x  , pos.y , pos.z )});
+		if (block == 1) {
+			GenFaceCommon(tGrass);
+		} else {
+			GenFaceCommon(tMud);
+		}
+	}
 
-		squareCount++;
+	void GenFaceLeft(Vector3 pos) {
+		NewVertices.AddRange(new Vector3[] {
+			new Vector3 (pos.x  , pos.y  , pos.z+1 ),
+			new Vector3 (pos.x, pos.y, pos.z ),
+			new Vector3 (pos.x, pos.y-1 , pos.z ),
+			new Vector3 (pos.x, pos.y-1  , pos.z+1 )});
+			GenFaceCommon(tMud);
+	}
+
+	void GenFaceFront(Vector3 pos) {
+		NewVertices.AddRange(new Vector3[] {
+			new Vector3 (pos.x, pos.y, pos.z ),
+			new Vector3 (pos.x + 1 , pos.y, pos.z ),
+			new Vector3 (pos.x + 1 , pos.y-1 , pos.z ),
+			new Vector3 (pos.x, pos.y-1 , pos.z )});
+			GenFaceCommon(tMud);
+	}
+
+	void GenFaceRight(Vector3 pos) {
+		NewVertices.AddRange(new Vector3[] {
+			new Vector3 (pos.x + 1 , pos.y , pos.z ),
+			new Vector3 (pos.x + 1 , pos.y, pos.z+1 ),
+			new Vector3 (pos.x + 1 , pos.y-1  , pos.z+1 ),
+			new Vector3 (pos.x + 1 , pos.y-1 , pos.z )});
+		GenFaceCommon(tMud);
+	}
+
+	void GenFaceBack(Vector3 pos) {
+		NewVertices.AddRange(new Vector3[] {
+			new Vector3 (pos.x + 1 , pos.y, pos.z+1 ),
+			new Vector3 (pos.x, pos.y, pos.z+1 ),
+			new Vector3 (pos.x, pos.y-1  , pos.z+1 ),
+			new Vector3 (pos.x + 1 , pos.y-1  , pos.z+1 )});
+		GenFaceCommon(tMud);
+	}
+
+	void GenFaceBottom(Vector3 pos) {
+		NewVertices.AddRange(new Vector3[] {
+			new Vector3 (pos.x, pos.y-1 , pos.z ),
+			new Vector3 (pos.x + 1 , pos.y-1 , pos.z ),
+			new Vector3 (pos.x + 1 , pos.y-1  , pos.z+1 ),
+			new Vector3 (pos.x, pos.y-1, pos.z+1 )});
+		GenFaceCommon(tMud);
+	}
+	
+	void GenBlock(int x, int y, int z, Vector2 texture1, Vector2 texture2) {
+		if (BlockAt(x,y,z) != 0) {
+			if (BlockAt(x, y+1, z) == 0)
+				GenFaceTop(new Vector3(x,y,z), BlockAt(x,y,z));
+			if (BlockAt(x, y-1, z) == 0)
+				GenFaceBottom(new Vector3(x,y,z));
+			if (BlockAt(x+1, y, z) == 0)
+				GenFaceRight(new Vector3(x,y,z));
+			if (BlockAt(x-1, y, z) == 0)
+				GenFaceLeft(new Vector3(x,y,z));
+			if (BlockAt(x, y, z+1) == 0)
+				GenFaceBack(new Vector3(x,y,z));
+			if (BlockAt(x, y, z-1) == 0)
+				GenFaceFront(new Vector3(x,y,z));
+		}
 	}
 	
 
 	void BuildMesh(){
-		Debug.Log("Chunk " + Position + " generating blocks");
+		//Debug.Log("Chunk " + Position + " generating blocks");
 		for (int x = 0; x < blocks.GetLength(0); x++) {
 			for (int z = 0; z < blocks.GetLength(2); z++) {
 				for (int y = 0; y < blocks.GetLength(1); y++) {
@@ -169,7 +190,7 @@ public class ChunkScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (renderer.enabled && Dirty) {
-			Debug.Log("Chunk " + Position + " dirty, generating mesh");
+			//Debug.Log("Chunk " + Position + " dirty, generating mesh");
 			Generate();
 			Dirty = false;
 		}
